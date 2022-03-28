ffe19861d52a992c9eddde2e942340d3a214fda7
Matteo Franzoi <anzo559@gmail.com>
2022-03-29 00:12:16 +0200

    Add two parameters in parametric problem

--- dispersion_surfaces_matlab.m
+++ dispersion_surfaces_matlab.m
@@ -2,10 +2,10 @@ clear all
 close all
 clc
 
-data = readtable('./data.csv');
+data = readtable('./export/data.csv');
 data = table2array(data);
 
 plot3(data(:,1), data(:,2), data(:,3))
 xlabel('kx*l1 [-]');
 ylabel('ky*l2 [-]');
-zlabel('omega [rad/s]');
+zlabel('Frequency [MHz]');
