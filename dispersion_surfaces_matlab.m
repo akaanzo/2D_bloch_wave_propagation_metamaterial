9ff8c543d927fcc6b353f26290f3bfa4062582d7
Matteo Franzoi <anzo559@gmail.com>
2022-04-06 18:56:17 +0200

    Plot dispersion surfaces

--- dispersion_surfaces_matlab.m
+++ dispersion_surfaces_matlab.m
@@ -2,10 +2,26 @@ clear all
 close all
 clc
 
-data = readtable('./export/data.csv');
+cd '~/git/2D_bloch_wave_propagation_metamaterial/'
+data = readtable('./export/modes.csv');
 data = table2array(data);
 
-plot3(data(:,1), data(:,2), data(:,3))
-xlabel('kx*l1 [-]');
-ylabel('ky*l2 [-]');
-zlabel('Frequency [MHz]');
+[nr, nc] = size(data);
+
+for i = 3:nc
+
+figure(i-2)
+tri=delaunay(data(:,1),data(:,2));
+trisurf(tri,data(:,1),data(:,2),data(:,i))
+title('Shape mode:', num2str(i-2))
+xlabel('kx*l/\pi [-]')
+ylabel('ky*l/\pi [-]')
+zlabel('\Omega = \omega*l/c [-]')
+grid on
+
+fig = sprintf('shape_mode_%d.jpg', i-2);
+cd '~/git/2D_bloch_wave_propagation_metamaterial/export/img/matlab/';
+saveas(figure(i-2), fig)
+
+end
+cd '~/git/2D_bloch_wave_propagation_metamaterial/'
